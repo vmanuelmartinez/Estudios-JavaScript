3ac897d8fe7a36d392aa94ad2107a9b5fdef6d73
victormanuelmartinez <victormmartinezf@gmail.com>
2022-09-19 18:34:18 -0300

    Ejemplos Ejercicios

---
+++ Ventanas.js
@@ -0,0 +1,13 @@
+// ALERTAS
+/*
+alert("Esta es mi alerta!");
+alert("Este es mi texto!")
+
+// Confirmación
+
+let ResultConfirm = confirm ("¿Deseas continuar?")
+console.log(ResultConfirm); */
+
+// INGRESO DE DATOS "PROMPT"
+let ResultPrompt = prompt("¿Que edad tienes?", 15)
+console.log(parseInt) (ResultPrompt)
