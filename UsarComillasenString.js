34b84347086e6b7f25902651c23340bb4f5607fe
victormanuelmartinez <victormmartinezf@gmail.com>
2022-10-22 21:48:15 -0300

    Subiendo el archivo que enseña como usar comillas dentro de un texto de una variable tipo String

---
+++ UsarComillasenString.js
@@ -0,0 +1,7 @@
+// Como colocar comillas "" dentro de un dato tipo STRING.
+
+
+/* En JavaScript, puede evitar que una comilla se considere como una comilla
+ de fin de cadena colocando una barra invertida ( \) delante de la comilla */
+
+const myStr = "Soy una cadena \"doble comilla\" dentro de \"doble comilla\"."
